9ad94a48c82035164f8e4089f4212489804a33d2
RuanYixin <74415464+17750977756@users.noreply.github.com>
2025-06-11 20:49:32 +0800

    Update functions.js

--- shuju/functions.js
+++ shuju/functions.js
@@ -38,23 +38,9 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">"; 
-	$("#clock").html(result);
+    var current = new Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var days = Math.floor(seconds / (3600 * 24));
+    var result = "第 <span class=\"digit\">" + days + "</span> 天";
+    $("#clock").html(result);
 }
